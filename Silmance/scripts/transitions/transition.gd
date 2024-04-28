ce6ef2d009ba8fa02342d308c8117de828e5b23b
JulioEvencio <julioigreja.dev@gmail.com>
2024-04-28 17:41:25 -0300

    added: Boot Splash and Transition

---
+++ Silmance/scripts/transitions/transition.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+@export var _animation_player : AnimationPlayer
+
+var _lambda : Callable
+
+func _ready() -> void:
+	_animation_player.play("fade_out")
+
+func start(callable : Callable) -> void:
+	_lambda = callable
+	_animation_player.play("fade_in")
+
+func _on_animation_player_animation_finished(anim_name : String) -> void:
+	match anim_name:
+		"fade_in":
+			_lambda.call()
+			_animation_player.play("fade_out")
